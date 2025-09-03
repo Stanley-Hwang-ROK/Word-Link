6f0a7043d61d8fcced1cd6f5103c9ccd236da39a
Stanley-Hwang-ROK <hsm8420@gmail.com>
2025-09-03 22:38:35 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'wordlink-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'manifest.json'
+  // 'icons/icon-192.png',
+  // 'icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      if (e.request.method === 'GET' && resp.status === 200 && resp.type === 'basic') {
+        const clone = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return resp;
+    }).catch(() => caches.match('index.html')))
+  );
+});
